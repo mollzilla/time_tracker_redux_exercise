d2bde4da717eff9bc5d900ba533be983a675e2b5
mollzilla <eymili@gmail.com>
2021-05-06 16:06:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+            Welcome to Mili's time tracking app
+        </h1>
+        <h2>This is going to take a while, I'm not well versed in anything algorithm and I don't want to follow tutorials.</h2>
       </header>
     </div>
   );
